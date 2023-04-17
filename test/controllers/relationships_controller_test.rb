cc69efd66d846ea62895ad01406d353f0f345843
manmohan.menon <manmohan.menon@gmail.com>
2023-04-18 03:51:39 +0900

    adding base test code,testing still in progress

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class RelationshipsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    User = delete.all #resetting users table
+    @user = User.find_or_create_by(name: 'Castle')
+    @friend = User.find_or_create_by(name: 'Kate')
+    @relationship = Relationship.create(follower: @user, followee: @friend)
+  end
+
+  test "should create relationship" do
+    new_friend = User.find_or_create_by(name: 'Martha')
+    assert_difference('Relationship.count') do
+      post relationships_url, params: { relationship: { follower_id: @user.id, followee_id: new_friend.id } }, as: :json
+    end
+    assert_response :created
+  end
+
+  test "should destroy relationship" do
+    assert_difference('Relationship.count', -1) do
+      delete relationship_url(@relationship), as: :json
+    end
+    assert_response :no_content
+  end
 end
--- test/controllers/sleep_records_controller_test.rb
+++ test/controllers/sleep_records_controller_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class SleepRecordsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    User = delete.all #resetting users table
+    @user = User.find_or_create_by(name: 'Castle')
+    @friend = User.find_or_create_by(name: 'Kate')
+    Relationship.create(follower: @user, followee: @friend)
+    @sleep_record = SleepRecord.create(user: @user, start_time: 1.day.ago, end_time: Time.now)
+  end
+
+  test "should create sleep record" do
+    assert_difference('SleepRecord.count') do
+    post sleep_records_url, params: { sleep_record: { user_id: @user.id, start_time: Time.now, end_time: Time.now } }, as: :json
+    end
+    assert_response :created
+  end
+
+  test "should not create sleep record without user id" do
+    assert_no_difference('SleepRecord.count') do
+    post sleep_records_url, params: { sleep_record: { start_time: Time.now, end_time: Time.now } }, as: :json
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "should get clocked in times" do
+    get clock_in_times_sleep_records_url, as: :json
+    assert_response :success
+    end
+    
+    test "should get friends records" do
+    get friends_records_sleep_records_url(@user.id), as: :json
+    assert_response :success
+    end
 end

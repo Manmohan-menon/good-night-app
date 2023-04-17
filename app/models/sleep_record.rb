8cd710ded871a841f79e133514a0434406808686
manmohan.menon <manmohan.menon@gmail.com>
2023-04-17 23:21:27 +0900

    added associations for each model

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,7 @@
 class SleepRecord < ApplicationRecord
   belongs_to :user
+
+  def duration
+    end_time - start_time
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :sleep_records, dependent: :destroy
+  has_many :followee_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'followee_id', dependent: :destroy
+  has_many :followees, through: :followee_relationships
+  has_many :followers, through: :follower_relationships
 end
